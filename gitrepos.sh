5712ff5f82d5d502f77ccd5ca1f0876eb2d6118d
Tobias Ericsson <tobias.ericsson@mobenga.com>
2018-08-31 14:30:52 +0200

    gitrepos

---
+++ gitrepos.sh
@@ -0,0 +1,7 @@
+
+git clone git@github.com:tobias-ericsson/docker.git
+git clone git@github.com:tobias-ericsson/tobias-ericsson.github.io.git
+git clone git@gitlab.com:t.ericsson/t.ericsson.gitlab.io.git
+git clone git@github.com:bjarke-bard/bjarke-bard.github.io.git
+
+
