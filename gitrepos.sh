603b5ed3b00b6b4d21df61d5c5acf60c8da6d5d9
Tobias Ericsson <tobias.ericsson@verisure.com>
2018-10-04 10:24:29 +0200

    gitrepos

--- gitrepos.sh
+++ gitrepos.sh
@@ -4,4 +4,9 @@ git clone git@github.com:tobias-ericsson/tobias-ericsson.github.io.git
 git clone git@gitlab.com:t.ericsson/t.ericsson.gitlab.io.git
 git clone git@github.com:bjarke-bard/bjarke-bard.github.io.git
 
+git clone git://github.com/joelthelion/autojump.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
 
