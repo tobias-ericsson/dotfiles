fbf5fe5b5341b0f1ffa40e0d1d28a0707d377d48
Tobias Ericsson <t.ericsson@gmail.com>
2019-09-24 10:55:19 +0200

    stuff

--- macOS/install/install.sh
+++ macOS/install/install.sh
@@ -8,6 +8,10 @@ brew install wget
 brew install git
 brew install autojump
 brew install terraform
+#command line YAML processor
+brew install yq
+#command line JSON processor
+brew install jq
 zsh.sh
 docker.sh
 kubernetes.sh
