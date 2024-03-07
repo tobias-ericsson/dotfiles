dbf696f43569af955cef604fba0826b02587a290
Tobias Ericsson <tobias.ericsson@powdrsoft.com>
2024-03-07 11:04:04 +0100

    git

--- gitrepos.sh
+++ gitrepos.sh
@@ -1,12 +1,10 @@
 
+cd ~/dev/me
 git clone git@github.com:tobias-ericsson/docker.git
 git clone git@github.com:tobias-ericsson/tobias-ericsson.github.io.git
 git clone git@gitlab.com:t.ericsson/t.ericsson.gitlab.io.git
 git clone git@github.com:bjarke-bard/bjarke-bard.github.io.git
-
-git clone git://github.com/joelthelion/autojump.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd -
 
 
 
