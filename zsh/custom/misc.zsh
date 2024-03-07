2d51d23feb27a3bbe07d93c3b5fa23f632d012ff
Tobias Ericsson <tobias.ericsson@mobenga.com>
2024-03-07 15:33:27 +0100

    gogo

--- zsh/custom/misc.zsh
+++ zsh/custom/misc.zsh
@@ -1,1 +1,3 @@
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
