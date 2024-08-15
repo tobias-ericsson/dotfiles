b3eb50847e2c3e3b3acabd9a22cd179abbe5a025
Tobias Ericsson <tobias.ericsson@neo4j.com>
2024-08-15 11:21:49 +0200

    path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,14 +1,18 @@
-PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-PATH="$HOME/go/bin:$PATH"
-PATH="/Applications/IntelliJ IDEA CE.app/Contents/MacOS:$PATH"
+#PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+#PATH="/Applications/IntelliJ IDEA CE.app/Contents/MacOS:$PATH"
+#PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS:$PATH"
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "\$HOME/bin" ] ; then
     PATH="\$HOME/bin:\$PATH"
 fi
 
-if [ -d "\$HOME/.local/bin" ] ; then
-    PATH="\$HOME/.local/bin:\$PATH"
+if [ -d "\$HOME/go/bin" ] ; then
+    PATH="\$HOME/go/bin:\$PATH"
+fi
+
+if [ -d "\$HOME/me/bin" ] ; then
+    PATH="\$HOME/me/bin:\$PATH"
 fi
 
 export PATH
