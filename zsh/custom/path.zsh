088c51f17f320b523132eeb19e4c044967e2e6b0
Tobias Ericsson <tobias.ericsson@powdrsoft.com>
2024-08-15 16:55:35 +0200

    path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,18 +1,8 @@
-#PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-#PATH="/Applications/IntelliJ IDEA CE.app/Contents/MacOS:$PATH"
-#PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS:$PATH"
 
-# set PATH so it includes user's private bin if it exists
-if [ -d "\$HOME/bin" ] ; then
-    PATH="\$HOME/bin:\$PATH"
-fi
-
-if [ -d "\$HOME/go/bin" ] ; then
-    PATH="\$HOME/go/bin:\$PATH"
-fi
-
-if [ -d "\$HOME/me/bin" ] ; then
-    PATH="\$HOME/me/bin:\$PATH"
+if [ -d "$HOME/bin" ] ; then
+  PATH="$HOME/bin:$PATH"
+  PATH="$HOME/go/bin:$PATH"
+  PATH="$HOME/me/bin:$PATH"
 fi
 
 export PATH
