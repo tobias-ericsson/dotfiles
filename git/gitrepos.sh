210db7edbf952632f8dac80c4207794a655dcb0f
Tobias Ericsson <tobias.ericsson@neo4j.com>
2024-08-28 15:15:35 +0200

    gitrepos

--- git/gitrepos.sh
+++ git/gitrepos.sh
@@ -1,10 +1,10 @@
 
-cd ~/dev/me
+cd ~/dev/me || exit
 git clone git@github.com:tobias-ericsson/docker.git
 git clone git@github.com:tobias-ericsson/tobias-ericsson.github.io.git
 git clone git@gitlab.com:t.ericsson/t.ericsson.gitlab.io.git
 git clone git@github.com:bjarke-bard/bjarke-bard.github.io.git
-cd -
+cd - || exit
 
 
 
