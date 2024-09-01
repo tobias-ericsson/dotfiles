aa740d2bf1d97c839ea496991295e68904f0c40a
Tobias Ericsson <tobias.ericsson@neo4j.com>
2024-09-01 14:36:32 +0200

    wip

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -4,4 +4,5 @@
 alias kgp="kubectl get pods"
 alias k="kubectl"
 alias cat="bat -pp"
-alias ls="eza -l"
+alias ls="eza -la --no-permissions --no-user --no-time -ssize"
+alias fzfp="fzf --preview 'bat --color always {}'"
