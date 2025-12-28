1fcf7d726a60e5d28f7b0e9583591a667de274a6
Tobias Ericsson <tobias.ericsson@neo4j.com>
2025-12-28 15:15:54 +0100

    zshrc

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -4,7 +4,8 @@ alias python="python3"
 alias pip="pip3"
 alias kgp="kubectl get pods"
 alias k="kubectl"
-alias cat="bat -pp"
+# ansi works with both light and dark background
+alias cat='bat --theme=ansi -pp'
 alias ls="eza -la"
 alias l="eza -l --no-permissions --no-user --no-time -ssize"
 alias fzfp="fzf --preview 'bat --color always {}'"
