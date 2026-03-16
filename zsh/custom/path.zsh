d47f761424c9326b097b1c9738ccf2335faaa4b3
Tobias Ericsson <tobias.ericsson_c@sandvik.com>
2026-03-16 14:51:52 +0100

    zshrc

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -3,9 +3,13 @@ if [ -d "$HOME/bin" ] ; then
   PATH="$HOME/bin:$PATH"
   PATH="$HOME/go/bin:$PATH"
   PATH="$HOME/me/bin:$PATH"
+  PATH="$HOME/.local/bin:$PATH"
 fi
 
 PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS":$PATH
+
+PATH="/opt/homebrew/bin:$PATH"
+PATH="/opt/homebrew/sbin:$PATH"
 PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
 
 PATH="/Users/tobias/program/pact/bin:$PATH"
